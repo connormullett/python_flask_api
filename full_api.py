78a1213fdbea82a90ea86fd8d3fa2ed294607ddf
connor <connormullett@gmail.com>
2019-01-11 00:51:15 -0500

    added foreign key implementation (could be done better)

--- full_api.py
+++ full_api.py
@@ -10,7 +10,6 @@ db = SQLAlchemy(app)
 
 
 # models for ORM
-"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -19,22 +18,32 @@ class User(db.Model):
     def __init__(self, username, email):
         self.username = username
         self.email = email
-"""
+
+    def to_json(self):
+        return {
+                'username': self.username,
+                'email': self.email
+                }
 
 
 class Language(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10), unique=True)
     framework = db.Column(db.String(10), unique=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, framework):
+    def __init__(self, name, framework, owner_id):
         self.name = name
         self.framework = framework
+        self.owner_id = owner_id
 
     def to_json(self):
         return {
                 'name': self.name,
-                'framework': self.framework
+                'framework': self.framework,
+                # queries for the owner_id given, grabs the object, and recursively
+                # calls to_json()
+                'posted_by': User.query.filter_by(id=self.owner_id).first().to_json()
                 }
 
 
@@ -52,9 +61,10 @@ def index():
         # sets local variables from request
         name = language['name']
         framework = language['framework']
+        owner_id = language['owner_id']
 
         # creates a new model object, stores it in db, and persists it
-        new_language = Language(name=name, framework=framework)
+        new_language = Language(name=name, framework=framework, owner_id=owner_id)
         db.session.add(new_language)
         db.session.commit()
 
@@ -108,6 +118,27 @@ def action_by_id(id):
     else:
         # flask handles this automatically, but why not have it for logics sake
         return jsonify({'status': 'Method not supported'})
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    user = request.get_json()
+
+    new_user = User(username=user['username'], email=user['email'])
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'status': '201'})
+
+
+@app.route('/user/<id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(404)
+
+    return jsonify(user.to_json())
 
 
 # runs the app if launched from cmd line, should not be used in production
